8d62d57a4542d15af4ced60623c3b65e3494b270
sabby <salilbhatt92@gmail.com>
2026-04-27 21:42:45 +0530

    Addming externa Folder

---
+++ demoOauth/src/main/resources/ouath.java
@@ -0,0 +1,16 @@
+import static io.restassured.RestAssured.*;
+
+
+
+public  class  ouathTest{
+
+    public static void main(String args[]){
+
+        given().
+        .formParams("client_id", "692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com")
+                .form
+
+    }
+
+
+}
